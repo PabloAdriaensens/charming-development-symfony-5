ba059462f53ec7f1288b30987af54fb0edb5a180
Pablo Adriaensens Albiñana <pablo.adriaensens@gmail.com>
2022-11-03 10:18:09 +0100

    [ADD] Generated controller

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController
+{
+
+    /**
+     * @Route("/")
+     */
+    public function homepage(): Response
+    {
+        return new Response('What a great controller');
+    }
+
+    /**
+     * @Route("/questions/{slug}")
+     */
+    public function show($slug): Response
+    {
+        return new Response(
+            sprintf(
+                'Future page to show the question "%s"!',
+                ucwords(str_replace('-', ' ', $slug))
+            )
+        );
+    }
+
+}
